d8aa7e1494381385bbf0300b05b2a8b7ebf6c698
mscharlat <mayerscharlat@Mayers-MacBook-Pro.local>
2014-07-21 16:15:12 -0400

    415

--- EllkayGoalieGame/Assets/Scripts/MoveGoalie.js
+++ EllkayGoalieGame/Assets/Scripts/MoveGoalie.js
@@ -14,16 +14,37 @@ function Update () {
 			var touchPosition:Vector2 = Input.GetTouch(0).position;
 			Debug.Log(touchPosition);
 			
-			if(touchPosition.x >= 0 && touchPosition.x <= 500 && touchPosition.y >= 0 && touchPosition.y <= 500) {
+			if(touchPosition.x >= 0 && touchPosition.x <= Screen.width/2 && touchPosition.y >= Screen.height/2 && touchPosition.y <= Screen.height) {
 				anim.SetBool("TL", true);
+			}
+			if(touchPosition.x >= Screen.width/2 && touchPosition.x <= Screen.width && touchPosition.y >= Screen.height/2 && touchPosition.y <= Screen.height) {
+				anim.SetBool("TR", true);
+			}
+			if(touchPosition.x >= Screen.width/2 && touchPosition.x <= Screen.width && touchPosition.y >= 0 && touchPosition.y <= Screen.height/2) {
+				anim.SetBool("BR", true);
+			}
+			if(touchPosition.x >= 0 && touchPosition.x <= Screen.width/2 && touchPosition.y >= 0 && touchPosition.y <= Screen.height/2) {
+				anim.SetBool("BL", true);
 			}
 		}
 
 }
 
-function OnTriggerEnter2D(other: Collider2D) {
+function OnTriggerEnter2D(other : Collider2D) {
 	 if(other.tag == "triggerTL") {
-	 	//anim.SetBool("TL", false);
-	 	Debug.Log("worked");
+	 	anim.SetBool("TL", false);
+	 	Debug.Log("trigger worked");
+	 }
+	 if(other.tag == "triggerTR") {
+	 	anim.SetBool("TR", false);
+	 	Debug.Log("trigger worked");
+	 }
+	 if(other.tag == "triggerBR") {
+	 	anim.SetBool("BR", false);
+	 	Debug.Log("trigger worked");
+	 }
+	 if(other.tag == "triggerBL") {
+	 	anim.SetBool("BL", false);
+	 	Debug.Log("trigger worked");
 	 }
 }
